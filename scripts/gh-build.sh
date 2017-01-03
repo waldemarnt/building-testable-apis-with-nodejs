920a831a248978cd86118ac668881be467ec2adb
Waldemar Neto <waldemarnt@gmail.com>
2017-01-02 22:12:43 -0200

    Add travis config

---
+++ scripts/gh-build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -o errexit
+
+# config
+git config --global user.email "ci@travis.com"
+git config --global user.name "Travis CI"
+
+# build
+npm run build
+
+# deploy
+git add .
+git commit -m "Build revision"
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" master > /dev/null 2>&1
