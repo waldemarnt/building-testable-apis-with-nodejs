af72f65283c31adff8189a1c8df765bab630764f
Waldemar Neto <waldemarnt@gmail.com>
2017-01-02 23:04:49 -0200

    Silents commit

--- scripts/gh-build.sh
+++ scripts/gh-build.sh
@@ -10,5 +10,5 @@ npm run build
 
 # deploy
 git add .
-git commit -m "Build revision"
-git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master &> /dev/null
+git commit -m "Build revision" &> /dev/null
+git push --force "https://${GITHUB_TOKEN}@github.com/${GITHUB_REPO}.git" HEAD:master > /dev/null 2>&1
